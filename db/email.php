a433dc073c215f18284c07ed47f74e1a961ded83
NqQuocHuy9701 <ng.quochuy9701.com>
2023-04-23 21:08:23 +0700

    update DATN_2023

---
+++ db/email.php
@@ -0,0 +1,11 @@
+<?php
+$config_email = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'huybet780@gmail.com',
+    'fullname' => 'Nguyễn Quốc Huy',
+    'password' => '09072001nqh',
+    'smtp_secure' => 'tls',
+    'port' => 587,
+    'mailtype' => 'html',
+    'charset' => 'UTF-8'
+);
